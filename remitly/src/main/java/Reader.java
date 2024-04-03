4e42a05544d63524fb4d4b9bf42788581be6687c
ZuzannaZimmer <225158@student.uek.krakow.pl>
2024-04-03 18:42:16 +0200

    add spaces

--- remitly/src/main/java/Reader.java
+++ remitly/src/main/java/Reader.java
@@ -6,22 +6,21 @@ import java.io.IOException;
 
 public class Reader {
     public static boolean readJson(String filePath){
+
         try {
 
             try (FileReader reader = new FileReader(filePath)) {
+
                 JSONObject jsonObject = new JSONObject(new JSONTokener(reader));
-
                 JSONArray statementArray = jsonObject.getJSONObject("PolicyDocument").getJSONArray("Statement");
-
                 JSONObject firstStatement = statementArray.getJSONObject(0);
-
                 String resourceValue = firstStatement.getString("Resource");
-
 
                 return !resourceValue.equals("*");
             }
-        }catch (IOException | JSONException e){
+        } catch (IOException | JSONException e){
             e.printStackTrace();
+
             return true;
         }
     }
