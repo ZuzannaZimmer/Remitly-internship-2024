01d8430e2aae5a763cebf2556abe24af57855492
ZuzannaZimmer <225158@student.uek.krakow.pl>
2024-04-03 18:32:51 +0200

    removed comments

--- remitly/src/main/java/Reader.java
+++ remitly/src/main/java/Reader.java
@@ -7,29 +7,18 @@ import java.io.IOException;
 public class Reader {
     public static boolean readJson(String filePath){
         try {
-            //create Reader object to read json file
-            FileReader reader = new FileReader(filePath);
 
-            try {
+            try (FileReader reader = new FileReader(filePath)) {
                 JSONObject jsonObject = new JSONObject(new JSONTokener(reader));
 
-                //find JSONArray from Statement key
                 JSONArray statementArray = jsonObject.getJSONObject("PolicyDocument").getJSONArray("Statement");
 
-                //find first object JSONObject from array (index 0)
                 JSONObject firstStatement = statementArray.getJSONObject(0);
 
-                //find "Resource" key's value
                 String resourceValue = firstStatement.getString("Resource");
 
 
-                if (resourceValue.equals("*")) {
-                    return false;
-                } else {
-                    return true;
-                }
-            } finally {
-                reader.close();
+                return !resourceValue.equals("*");
             }
         }catch (IOException | JSONException e){
             e.printStackTrace();
